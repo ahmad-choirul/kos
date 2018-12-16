80ea462485c2b94b945b08da6a7b131f5406fbf4
syamsrachman <syamsrachman@gmail.com>
2018-12-16 23:32:59 +0700

    detail kos

--- application/views/detailkos.php
+++ application/views/detailkos.php
@@ -207,7 +207,6 @@
     </div>
   </div>
 </div>
-
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -215,7 +214,7 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner align-bottom">
-    
+
     <div class="carousel-item active">
       <img class="d-block w-100" src="assets/img/kos/1.jpg" alt="First slide">
     </div>
